e5e79f4a5b1caeb70eae4936ebd5c326b35579bf
614uc0 <glaucomv@gmail.com>
2023-06-24 12:23:23 -0300

    Initial commit

---
+++ app/src/main/java/devandroid/glaucomv/appgaseta/view/GasEtaActivity.java
@@ -0,0 +1,20 @@
+package devandroid.glaucomv.appgaseta.view;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import devandroid.glaucomv.appgaseta.R;
+
+public class GasEtaActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_gaseta);
+
+    }
+}
